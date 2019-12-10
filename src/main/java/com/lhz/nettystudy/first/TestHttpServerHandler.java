392b67053f136c6b853f38c5158eb63f78f26263
jerax <q14341319@gamil.com>
2019-12-10 15:19:26 +0800

    init

---
+++ src/main/java/com/lhz/nettystudy/first/TestHttpServerHandler.java
@@ -0,0 +1,70 @@
+package com.lhz.nettystudy.first;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
+
+/**
+ * @author JerAxxxxxxx
+ * @version Revision 1.0.0
+ * @date 2019/12/10 14:02
+ */
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    private static final String ICON = "/favicon.ico";
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest) msg;
+            System.out.println("请求方法名" + httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            if (ICON.equals(uri.getPath())) {
+                System.out.println("请求favicon.ico");
+                return;
+            }
+            ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK, content);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            ctx.writeAndFlush(response);
+        }
+    }
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel Active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel Registered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler Added");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel Inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel Unregistered");
+        super.channelUnregistered(ctx);
+    }
+}
---
+++ src/main/java/com/lhz/nettystudy/first/TestServerInitializer.java
@@ -0,0 +1,20 @@
+package com.lhz.nettystudy.first;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author JerAxxxxxxx
+ * @version Revision 1.0.0
+ * @date 2019/12/10 11:41
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
