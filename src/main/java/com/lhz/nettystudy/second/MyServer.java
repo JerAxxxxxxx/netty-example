fd474d1fe04b3b94849e94cd64e4c53a9c679597
JerAxxxxxxx <q14341319@gmail.com>
2020-01-13 15:12:40 +0800

    Last

--- src/main/java/com/lhz/nettystudy/second/MyServer.java
+++ src/main/java/com/lhz/nettystudy/second/MyServer.java
@@ -5,13 +5,15 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * @author JerAxxxxxxx
  * @version Revision 1.0.0
  * @date 2019/12/10 15:38
  */
-@SuppressWarnings("all")
+@SuppressWarnings("Duplicate")
 public class MyServer {
     public static void main(String[] args) throws InterruptedException {
         //完成转发  默认线程数处理器的2倍   一般开发中设置成1
@@ -21,6 +23,7 @@ public class MyServer {
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.WARN))
                     .childHandler(new MyServerInitializer());
 
             ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
